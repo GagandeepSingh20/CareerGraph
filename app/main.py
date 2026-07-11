f3e96ea2cef5b8248b5157f9fafd9a8a7e0c8155
GagandeepSingh20 <gchoudhary16555@gmail.com>
2026-07-11 21:53:43 +0530

    Add validated candidate creation endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel , EmailStr , AnyUrl, Field
+from typing import List , Optional , Annotated
 
 app=FastAPI()
+
+class Candidate(BaseModel):
+      
+      name: Annotated [str, Field(max_length=100, title="Name of the Candidate", description="Enter your Full Name")]
+
+      age: Annotated [int, Field(gt=0,title="Age of the Candidate", description="Enter your Age")]
+
+      email: Annotated [EmailStr,Field(title="Email of the Candidate", description="Enter your valid Email")]
+
+      Linkedin_url: Annotated [AnyUrl, Field(title="Linkedin of the Candidate", description="Enter your Linkedin url")]
+      
+      skills: List[str] | None = None
+      experience: int
+      
 
 @app.get("/")
 def hello():
@@ -10,5 +26,6 @@ def hello():
 def health_check():
     return {"status":"Server is running in Okay condition"}
 
-
-
+@app.post("/candidates/")
+async def candidate_creation(candidates:Candidate):
+    return candidates
