cca5ef4cac5ab7960985970c5dc269ef3dcfb5de
GagandeepSingh20 <gchoudhary16555@gmail.com>
2026-07-13 00:48:17 +0530

    Add models.py

---
+++ app/models.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime, Float, Integer, JSON, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
+def current_utc_time() -> datetime:
+    return datetime.now(timezone.utc)
+
+
+class CandidateDB(Base):
+
+    __tablename__ = "candidates"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key = True,
+        index = True,
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        nullable = False,
+    )
+
+    age: Mapped[int] = mapped_column(
+        Integer,
+        nullable = False,
+    )
+
+    cgpa: Mapped[float] = mapped_column(
+        Float,
+        nullable = False,
+    )
+
+    gender: Mapped[str] = mapped_column(
+        String,
+        nullable = False,
+    )
+
+    email: Mapped[str] = mapped_column(
+        String,
+        unique = True,
+        index = True,
+        nullable = False,
+    )
+
+    linkedin_url: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        nullable = False,
+    )
+
+    skills: Mapped[list[str] | None] = mapped_column(
+        JSON,
+        nullable=True,
+    )
+
+    experience: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+    )
+
+    graduation_year: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=current_utc_time,
+        nullable=False,
+    )
