01727110a33b6b97f1aec0c080c7e050a95965b8
GagandeepSingh20 <gchoudhary16555@gmail.com>
2026-07-20 19:43:49 +0530

    Added UserDB

---
+++ app/UserDB.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime, Float, Integer, String, JSON
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
