c1bee9d5e163d9375935477998a5d642c9e8130e
GagandeepSingh20 <gchoudhary16555@gmail.com>
2026-07-20 19:57:31 +0530

    Added UserDB class
    Added userdb

--- app/user.py
+++ app/user.py
@@ -1,7 +1,48 @@
 from datetime import datetime, timezone
 
-from sqlalchemy import DateTime, Float, Integer, String, JSON
+from sqlalchemy import DateTime, Float, Integer, String, Boolean, JSON
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .database import Base
 
+def current_utc_time() -> datetime:
+    return datetime.now(timezone.utc)
+
+
+class UserDB(Base):
+
+    __tablename__ = "users"
+
+    id: Mapped [int] = mapped_column(
+        Integer,
+        primary_key= True,
+        index = True,
+    )
+
+    email: Mapped[str] = mapped_column(
+        String,
+        unique = True,
+        index = True,
+        nullable = False,
+    )
+
+    hashed_password: Mapped[str] = mapped_column(
+        String,
+        index = True,
+        nullable = False,
+    )
+
+    role: Mapped[str] = mapped_column(
+        String,
+        nullable = False,
+    )
+
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime,
+        nullable= False
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable= False
+    )
